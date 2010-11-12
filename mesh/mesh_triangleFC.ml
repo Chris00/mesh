aafb549a020f044bf52ed170ae336d9c8ce2caff
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2010-11-12 23:26:24 +0100

    Use a buffer to accumulate Triangle's switches.

--- mesh/mesh_triangleFC.ml
+++ mesh/mesh_triangleFC.ml
@@ -13,66 +13,72 @@ let triangulate ?(delaunay=true) ?min_angle ?max_area
   if Array1.dim mesh#point_marker > 0
     && Array1.dim mesh#point_marker < NCOLS(mesh#point) then
     invalid_arg("dim mesh#point_marker < " ^ COLS ^ " mesh#point");
-  let switches = default_switches in
+  let switches = Buffer.create 20 in
+  Buffer.add_string switches default_switches;
   (* Check for PSLG *)
-  let switches =
-    if pslg then begin
-      if NCOLS(mesh#segment) > 0 then begin
-        if NROWS(mesh#segment) <> 2 then invalid_arg(ROWS ^ " segment <> 2");
-        if Array1.dim mesh#segment_marker > 0
-          && Array1.dim mesh#segment_marker < NCOLS(mesh#segment) then
-          invalid_arg("dim mesh#segment_marker < " ^ COLS ^ " mesh#segment");
-      end;
-      if not refine then begin
-        if NCOLS(mesh#hole) > 0 && NROWS(mesh#hole) <> 2 then
-          invalid_arg(ROWS ^ " hole <> 2");
-        if NCOLS(mesh#region) > 0 && NROWS(mesh#region) <> 4 then
-          invalid_arg(ROWS ^ " region <> 4");
-      end;
-      if NCOLS(mesh#segment) = 0 then switches ^ "pc"
-      else switches ^ "p"
-    end else switches in
+  if pslg then begin
+    if NCOLS(mesh#segment) > 0 then begin
+      if NROWS(mesh#segment) <> 2 then invalid_arg(ROWS ^ " segment <> 2");
+      if Array1.dim mesh#segment_marker > 0
+        && Array1.dim mesh#segment_marker < NCOLS(mesh#segment) then
+        invalid_arg("dim mesh#segment_marker < " ^ COLS ^ " mesh#segment");
+    end;
+    if not refine then begin
+      if NCOLS(mesh#hole) > 0 && NROWS(mesh#hole) <> 2 then
+        invalid_arg(ROWS ^ " hole <> 2");
+      if NCOLS(mesh#region) > 0 && NROWS(mesh#region) <> 4 then
+        invalid_arg(ROWS ^ " region <> 4");
+    end;
+    if NCOLS(mesh#segment) = 0 then Buffer.add_string switches "pc"
+    else Buffer.add_char switches 'p'
+  end;
   (* Check for refinement -- triangles *)
-  let switches =
-    if refine then begin
-      if NCOLS(mesh#triangle) > 0 then begin
-        if NROWS(mesh#triangle) < 3 then
-          invalid_arg(ROWS ^ " mesh#triangle < 3");
-        if NROWS(mesh#triangle_attribute) > 0
-          && NCOLS(mesh#triangle_attribute) < NCOLS(mesh#triangle) then
-          invalid_arg(COLS ^ " mesh#triangle_attribute < "
-                      ^ COLS ^ " mesh#triangle");
-      end;
-      switches ^ "r"
-    end else switches in
+  if refine then begin
+    if NCOLS(mesh#triangle) > 0 then begin
+      if NROWS(mesh#triangle) < 3 then
+        invalid_arg(ROWS ^ " mesh#triangle < 3");
+      if NROWS(mesh#triangle_attribute) > 0
+        && NCOLS(mesh#triangle_attribute) < NCOLS(mesh#triangle) then
+        invalid_arg(COLS ^ " mesh#triangle_attribute < "
+                    ^ COLS ^ " mesh#triangle");
+    end;
+    Buffer.add_char switches 'r'
+  end;
   (* Check triangle_area *)
-  let switches, triangle_area = match triangle_area with
-    | None -> switches, Array1.create float64 layout 0
+  let triangle_area = match triangle_area with
+    | None -> Array1.create float64 layout 0
     | Some a ->
       if Array1.dim a < NCOLS(mesh#triangle) then
         invalid_arg("dim triangle_area < " ^ COLS ^ " mesh#triangle");
-      (switches ^ "a", a) in
+      Buffer.add_char switches 'a';
+      a in
   (* Other switches *)
-  let switches = if delaunay then switches ^ "D" else switches in
-  let switches = match min_angle with
-    | None -> switches
-    | Some a -> switches ^ "q" ^ (string_of_float a) in
-  let switches = match max_area with
-    | None -> switches
-    | Some a -> switches ^ "a" ^ (string_of_float a) in
-  let switches = match max_steiner with
-    | None -> switches
-    | Some a -> switches ^ "S" ^ (string_of_int a) in
-  let switches = if voronoi then switches ^ "v" else switches in
-  let switches = if edge then switches ^ "e" else switches in
-  let switches = if neighbor then switches ^ "n" else switches in
-  let switches = if subparam then switches ^ "o2" else switches in
-  let switches = if debug then switches else switches ^ "Q" in
+  if delaunay then Buffer.add_char switches 'D';
+  (match min_angle with
+  | None -> ()
+  | Some a ->
+    Buffer.add_char switches 'q';
+    Buffer.add_string switches (string_of_float a));
+  (match max_area with
+  | None -> ()
+  | Some a ->
+    Buffer.add_char switches 'a';
+    Buffer.add_string switches (string_of_float a));
+  (match max_steiner with
+  | None -> ()
+  | Some a ->
+    Buffer.add_char switches 'S';
+    Buffer.add_string switches (string_of_int a));
+  if voronoi then Buffer.add_char switches 'v';
+  if edge then Buffer.add_char switches 'e';
+  if neighbor then Buffer.add_char switches 'n';
+  if subparam then Buffer.add_string switches "o2";
+  if not debug then Buffer.add_char switches 'Q';
   (* Call triangle and build the resulting objects *)
   let point, point_attribute, point_marker, triangle, triangle_attribute,
     neighbor, segment, segment_marker, edge, edge_marker,
     vor_point, vor_point_attribute, vor_edge, vor_normal =
-    triangle switches mesh triangle_area in
+    triangle (Buffer.contents switches) mesh triangle_area in
   let mesh_out : layout t =
     (object
       method point               = point
