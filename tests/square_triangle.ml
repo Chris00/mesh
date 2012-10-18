17816031dacd2ea7a6b93905b6e0c26851e9055c
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2012-10-18 23:48:13 +0200

    Test that "triunsuitable" is working when generating meshes.

--- tests/square_triangle.ml
+++ tests/square_triangle.ml
@@ -4,23 +4,26 @@ open Printf
 
 let pi = 4. *. atan 1.
 
-let mesh_square =
+let pslg =
   let pt = Array2.of_array float64 fortran_layout
     [| [| 0.; 1.; 1.; 0. |];   (* x *)
        [| 0.; 0.; 1.; 1. |] |] (* y *) in
   let edges = Array2.of_array int fortran_layout
     [| [| 1; 2; 3; 4 |];   (* start *)
        [| 2; 3; 4; 1 |] |] (* end *) in
-  let plsg = object
+  object
     inherit [_] Mesh_triangle.pslg fortran_layout
     method point = pt
     method segment = edges
-  end in
-  fst(Mesh_triangle.triangulate plsg ~max_area:0.0005)
+  end
 
 
 let () =
-  printf "The mesh has %i nodes.\n%!" (Array2.dim2 mesh_square#point);
+  let max_area = 0.0005 in
+  let mesh = fst(Mesh_triangle.triangulate pslg ~max_area) in
+  printf "The mesh has %i nodes.\n%!" (Array2.dim2 mesh#point);
   printf "Press 'q' on its window to quit the mesh display.%!";
-  Mesh_display.display mesh_square;
+  Mesh_display.display mesh;
+  let triunsuitable x1 y1 x2 y2 x3 y3 area = area > max_area in
+  Mesh_display.display (fst(Mesh_triangle.triangulate pslg ~triunsuitable));
   printf "\n";
