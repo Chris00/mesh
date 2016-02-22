2a3d6b7052104d2ec37c856aa7dea4b1b8d56252
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2016-02-22 05:17:39 +0100

    Enable a good presentation by default for Scilab output

--- src/meshFC.ml
+++ src/meshFC.ml
@@ -131,10 +131,14 @@ let scilab (mesh: mesh) (z: vec) fname =
   fprintf fh "// Run in Scilab with: exec('%s')\n\
               // Written by the OCaml Mesh module.\n\
               // mesh: %i triangles, %i points.\n\
-              ocaml = struct('x', fscanfMat('%s'), 'y', fscanfMat('%s'), \
+              ocaml = struct('f', scf(), 'e', null, \
+                             'x', fscanfMat('%s'), 'y', fscanfMat('%s'), \
                              'z', fscanfMat('%s'));\n\
               clf();\n\
-              plot3d(ocaml.x, ocaml.y, ocaml.z, theta=70, alpha=10)\n"
+              plot3d1(ocaml.x, ocaml.y, ocaml.z, theta=70, alpha=10);\n\
+              ocaml.e = gce();\n\
+              ocaml.e.hiddencolor = -1;\n\
+              ocaml.f.color_map = jetcolormap(32);\n"
            sci (NCOLS(triangle)) (NCOLS(pt)) xf yf zf;
   close_out fh;
   let save_mat fname coord =
